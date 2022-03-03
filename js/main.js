4ee672b6ebf27cbda81b1669980366ba82079c53
Bin.T28 <nguyenhoangnhannn93@gmail.com>
2022-03-03 19:49:33 +0700

    website

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const check1 = document.getElementById('nav1');
+const check2 = document.getElementById('btn-tel');
+const check3 = document.getElementById('btn-6');
+
+check1.onclick = function(){
+    alert('Chức năng hiện đang được phát triển');
+}
+
+check2.onclick =function() {
+    alert('Chức năng hiện đang được phát triển');
+}
+
+check3.onclick =function() {
+    alert('Chức năng hiện đang được phát triển');
+}
